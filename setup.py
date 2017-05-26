a1a9ea7c5d3b193a78d1d2c16fc821134d318a56
Gustavo Bezerra <gusutabopb@gmail.com>
2017-05-26 12:21:27 +0900

    Fix requirements

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='pytrthree',
       url='https://github.com/plugaai/pytrthree',
       packages=['pytrthree'],
       license='GPL',
-      install_requires=['zeep', 'pytest', 'pandas', 'pyyaml'],
+      install_requires=['zeep', 'pytest', 'pandas', 'pyyaml', 'numpy'],
       classifiers=[
           'Intended Audience :: Developers',
           'Intended Audience :: Science/Research',
